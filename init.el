2e6ec114be617c82c67e9227d083fb60792d119f
Kun Li <likun@msn.cn>
2023-01-19 18:57:47 +0800

    Update init.el

--- init.el
+++ init.el
@@ -5,25 +5,37 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(current-language-environment "UTF-8")
- '(custom-enabled-themes nil)
+ '(custom-enabled-themes '(zenburn))
+ '(custom-safe-themes
+   '("e4486d0ad184fb7511e391b6ecb8c4d7e5ab29e2d33bc65403e2315dbacaa4aa" default))
+ '(global-display-line-numbers-mode t)
  '(package-selected-packages
-   '(page-break-lines projectile all-the-icons dashboard which-key rust-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Megatops ProCoder 1.0" :foundry "raster" :slant normal :weight normal :height 105 :width normal)))))
+   '(cnfonts markdown-mode auto-org-md page-break-lines projectile all-the-icons dashboard which-key rust-mode))
+ '(tool-bar-mode nil))
 
 
 ;;========================================
-;; F2 打开配置；F3 刷新配置
+;; 打开配置函数
 ;;========================================
 (defun open-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
-(global-set-key (kbd "<f2>") 'open-init-file)
-(global-set-key (kbd "<f3>") 'eval-buffer)
+
+
+;;========================================
+;; C+J
+;;========================================
+(global-set-key (kbd "C-J") '(lambda ()
+  (interactive)
+  (move-end-of-line 1)
+  (newline)))
+
+;;========================================
+;; 简写词汇
+;;========================================
+(setq-default abbrev-mode t)
+(read-abbrev-file "~/.emacs.d/.abbrev_defs")
+(setq save-abbrevs t)
 
 
 ;;========================================
@@ -42,18 +54,19 @@
 ;;===========================================================================
 ;; 垃圾回收，在 Windows Emacs 25 版本会频繁触发垃圾回收，以下解决此问题
 ;;===========================================================================
-(when (eq system-type 'windows-nt)
-  (setq gc-cons-threshold (* 512 1024 1024))
-  (setq gc-cons-percentage 0.5)
-  (run-with-idle-timer 5 t #'garbage-collect)
-  ;; (setq garbage-collection-messages t)  ; 调试垃圾回收信息
-)
+;;(when (eq system-type 'windows-nt)
+;;  (setq gc-cons-threshold (* 512 1024 1024))
+;;  (setq gc-cons-percentage 0.5)
+;;  (run-with-idle-timer 5 t #'garbage-collect)
+;;  ;; (setq garbage-collection-messages t)  ; 调试垃圾回收信息
+;;)
 
 
 ;;========================================
 ;; encoding
 ;;========================================
 (set-language-environment 'Chinese-GBK)
+(prefer-coding-system 'gb18030)
 (prefer-coding-system 'utf-8-auto)
 
 ;; (setq locale-coding-system 'utf-8)
@@ -81,29 +94,33 @@
 (setq cursor-type 'bar)
 (icomplete-mode 1)
 (global-auto-revert-mode 1)
-;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
-(set-face-attribute 'default nil :height 160)
-;; Window Size
-(set-frame-font "Megatops ProCoder 1.0")
+;;;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
+;;(set-face-attribute 'default nil :height 160)
+;;;; Window Size
+;;(set-frame-font "Megatops ProCoder 1.0")
 (if (not (eq window-system nil))
-    (progn
-      ;; top, left ... must be integer
-      (add-to-list 'default-frame-alist
-                   (cons 'top  (/ (x-display-pixel-height) 10)))
-      (add-to-list 'default-frame-alist
-                   (cons 'left (/ (x-display-pixel-width) 10)))
-      (add-to-list 'default-frame-alist
-                   (cons 'height (/ (* 4 (x-display-pixel-height))
-                                    (* 5 (frame-char-height)))))
-      (add-to-list 'default-frame-alist
-                   (cons 'width (/ (* 4 (x-display-pixel-width))
-                                   (* 5 (frame-char-width)))))))
+  (progn
+    ;; top, left ... must be integer
+    (add-to-list 'default-frame-alist
+      (cons 'top  (/ (x-display-pixel-height) 10)))
+    (add-to-list 'default-frame-alist
+      (cons 'left (/ (x-display-pixel-width) 10)))
+    (add-to-list 'default-frame-alist
+      (cons 'height (/ (* 4 (x-display-pixel-height))
+                       (* 5 (frame-char-height)))))
+    (add-to-list 'default-frame-alist
+      (cons 'width (/ (* 4 (x-display-pixel-width))
+                      (* 5 (frame-char-width)))))))
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1) ((control) . nil)))
 (setq mouse-wheel-progressive-speed nil)
 
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'zenburn t)
+
+(desktop-save-mode 1)
+(save-place-mode 1)
+
 
 
 ;;========================================
@@ -119,22 +136,37 @@
 (setq command-line-default-directory "d:/code/emacs/")
 
 
+(add-to-list 'load-path "~/.emacs.d/kl-settings/")
+(require 'nodesktopsaveorsessionwarning)
+;; (require 'google)
+;; (setq google-license-key "" ; optional
+;;       google-referer "https://www.google.com") ; required!
+;; (google-search-video "rickroll")
+
+
+(require 'cnfonts)
+;; 让 cnfonts 在 Emacs 启动时自动生效。
+(cnfonts-mode 1)
+;; 添加两个字号增大缩小的快捷键
+(define-key cnfonts-mode-map (kbd "C--") #'cnfonts-decrease-fontsize)
+(define-key cnfonts-mode-map (kbd "C-=") #'cnfonts-increase-fontsize)
+
 ;;========================================
-;; dashboard 因拖慢，已禁用
+;; dashboard 会拖慢
 ;;========================================
-;; (require 'dashboard)
-;; (dashboard-setup-startup-hook) 
-;; (setq dashboard-banner-logo-title "Welcome to Emacs Dashboard")
-;; (setq dashboard-startup-banner 1)
-;; (setq dashboard-center-content t)
-;; (setq dashboard-show-shortcuts nil)
-;; (setq dashboard-items '((recents  . 5)
-;;                         (bookmarks . 5)
-;;                         (projects . 5)
-;;                         (agenda . 5)
-;;                         (registers . 5)))
-;; (setq dashboard-set-heading-icons t)
-;; (setq dashboard-set-file-icons t)
+(require 'dashboard)
+(dashboard-setup-startup-hook) 
+(setq dashboard-banner-logo-title "Welcome to Emacs Dashboard")
+(setq dashboard-startup-banner 1)
+(setq dashboard-center-content t)
+(setq dashboard-show-shortcuts nil)
+(setq dashboard-items '((recents  . 5)
+                        (bookmarks . 5)
+                        (projects . 5)
+                        (agenda . 5)
+                        (registers . 5)))
+(setq dashboard-set-heading-icons t)
+(setq dashboard-set-file-icons t)
 
 ;;========================================
 ;; neotree
@@ -169,17 +201,15 @@
   "insert the current date and time into current buffer. Uses `current-date-time-format' for the formatting the date/time."
   (interactive)
   (insert "==========\n")
-  ;(insert (let () (comment-start)))
+  ;; (insert (let () (comment-start)))
   (insert (format-time-string current-date-time-format (current-time)))
-  (insert "\n")
-)
+  (insert "\n"))
 
 (defun insert-current-time ()
   "insert the current time (1-week scope) into the current buffer."
   (interactive)
   (insert (format-time-string current-time-format (current-time)))
-  (insert "\n")
-)
+  (insert "\n"))
 
 
 ;;=======================================
@@ -193,17 +223,92 @@
 ;; 注释
 ;;========================================
 (defun comment-or-uncomment-region-or-line ()
-    "Comments or uncomments the region or the current line if there's no active region."
-    (interactive)
-    (let (beg end)
-        (if (region-active-p)
-            (setq beg (region-beginning) end (region-end))
-            (setq beg (line-beginning-position) end (line-end-position)))
-        (comment-or-uncomment-region beg end)))
+  "Comments or uncomments the region or the current line if there's no active region."
+  (interactive)
+  (let (beg end)
+    (if (region-active-p)
+      (setq beg (region-beginning) end (region-end))
+      (setq beg (line-beginning-position) end (line-end-position)))
+    (comment-or-uncomment-region beg end)))
+
+(defun copy-line (&optional arg)
+  "Save current line into Kill-Ring without mark the line"
+  (interactive "P")
+  (let ((beg (line-beginning-position))
+    (end (line-end-position arg)))
+  (copy-region-as-kill beg end)))
 
 
+(defun copy-word (&optional arg)
+  "Copy words at point"
+  (interactive "P")
+  (let ((beg (progn (if (looking-back "[a-zA-Z0-9]" 1) (backward-word 1)) (point))) 
+    (end (progn (forward-word arg) (point))))
+  (copy-region-as-kill beg end)))
 
-(global-set-key "\C-c\C-d" 'insert-current-date-time)
-(global-set-key "\C-c\C-t" 'insert-current-time)
-(global-set-key "\C-c\C-c" 'comment-or-uncomment-region-or-line)
 
+(defun copy-paragraph (&optional arg)
+  "Copy paragraphes at point"
+  (interactive "P")
+  (let ((beg (progn (backward-paragraph 1) (point))) 
+	(end (progn (forward-paragraph arg) (point))))
+  (copy-region-as-kill beg end)))
+
+
+(defun quick-copy-line ()
+   "Copy the whole line that point is on and move to the beginning of the next line. Consecutive calls to this command append each line to the kill-ring."
+   (interactive)
+   (let ((beg (line-beginning-position 1))
+         (end (line-beginning-position 2)))
+     (if (eq last-command 'quick-copy-line)
+	 (kill-append (buffer-substring beg end) (< end beg))
+       (kill-new (buffer-substring beg end))))
+   (beginning-of-line 2))
+
+
+(defun quick-cut-line ()
+  "Cut the whole line that point is on.  Consecutive calls to this command append each line to the kill-ring."
+  (interactive)
+  (let ((beg (line-beginning-position 1))
+	(end (line-beginning-position 2)))
+    (if (eq last-command 'quick-cut-line)
+	(kill-append (buffer-substring beg end) (< end beg))
+      (kill-new (buffer-substring beg end)))
+    (delete-region beg end))
+  (beginning-of-line 1)
+  (setq this-command 'quick-cut-line))
+
+
+;; copy region or whole line
+(global-set-key "\M-w"
+(lambda ()
+  (interactive)
+  (if mark-active
+      (kill-ring-save (region-beginning)
+      (region-end))
+    (progn
+     (kill-ring-save (line-beginning-position)
+     (line-end-position))
+     (message "copied line")))))
+
+
+;; kill region or whole line
+(global-set-key "\C-w"
+(lambda ()
+  (interactive)
+  (if mark-active
+      (kill-region (region-beginning)
+   (region-end))
+    (progn
+     (kill-region (line-beginning-position)
+  (line-end-position))
+     (message "killed line")))))
+
+
+(global-set-key (kbd "<f2>") 'open-init-file)
+(global-set-key (kbd "<f3>") 'eval-buffer)
+(global-set-key (kbd "C-c C-d") 'insert-current-date-time)
+(global-set-key (kbd "C-c C-t") 'insert-current-time)
+(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region-or-line)
+(global-set-key (kbd "<f11>") 'quick-copy-line)
+(global-set-key (kbd "M-<f11>") 'quick-cut-line)
